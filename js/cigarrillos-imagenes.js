8b494e7f226814b3749b1074b8228854aa401607
alzologisticacba <compras@pellamayorista.com>
2025-08-25 14:52:38 -0300

    Cambio a png

--- js/cigarrillos-imagenes.js
+++ js/cigarrillos-imagenes.js
@@ -1,9 +1,9 @@
 // js/cigarrillos-images.js
 window.CIG_IMG_PAGES = [
   "img/cigarrillos/1.png",
-  "img/cigarrillos/2.webp",
-  "img/cigarrillos/3.webp",
-  "img/cigarrillos/4.webp",
-  "img/cigarrillos/5.webp",
-  "img/cigarrillos/6.webp"
+  "img/cigarrillos/2.png",
+  "img/cigarrillos/3.png",
+  "img/cigarrillos/4.png",
+  "img/cigarrillos/5.png",
+  "img/cigarrillos/6.png"
 ];
